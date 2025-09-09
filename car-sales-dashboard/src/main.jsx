d6adf80dfa8d581e9bab5c86f04e6a85abdbe719
abdullah-binmadhi <89339365+abdullah-binmadhi@users.noreply.github.com>
2025-09-09 13:44:32 +0800

    Fix blank page issue by correcting base path configuration

--- car-sales-dashboard/src/main.jsx
+++ car-sales-dashboard/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="./Car-Sales-Dashboard">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
